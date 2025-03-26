3749abd89870411b283e92b449686f1614bce695
Nikhilesh Bisht <nikhilesh.bisht@Polestarllp.local>
2025-03-27 00:33:14 +0530

    date formate change dd/mm/yyyy

--- src/pages/credit-note/CreditNote.tsx
+++ src/pages/credit-note/CreditNote.tsx
@@ -1159,7 +1159,7 @@ const CreditNoteMaster = () => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, "0");
         const day = String(date.getDate()).padStart(2, "0");
-        return `${year}/${month}/${day}`;
+        return `${day}/${month}/${year}`;
     };
 
     function concatAddresses(address1: string, address2: string, address3: string) {
--- src/pages/master/FinancialYearMaster.tsx
+++ src/pages/master/FinancialYearMaster.tsx
@@ -317,12 +317,12 @@ const FinancialYearMaster = () => {
 
   const formatDate = (dateString: any) => {
     console.log("dateString",dateString)
-    if(!dateString)return null
+    if(!dateString)return null;
     const date = new Date(dateString);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}/${month}/${day}`;
+    return `${day}/${month}/${year}`;
   };
 
   const parseDateString = (dateString: any) => {
